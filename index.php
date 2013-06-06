f571bf8c17b2d7a2eb74666c102aeafea65c0ea9
eldeazul <hector.hgtr@outlook.com>
2013-06-06 16:05:47 -0500

    index.php
    dgsdfsdfdf bgdb g g g

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<title>Voluntariado CANACINTRA COLIMA</title>
+<meta name="description" content=""/>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<link rel="stylesheet"  href="estilo_canacintra.css">
+<link rel="stylesheet"  href="normalize.css">
+</head>
+
+<body>
+<div id="contenedor">
+<header>
+  <figure id="logo">
+	<a href="index.php"><img src="archivos/logo_canacintra_colima.png" alt="Logotipo CANACINTRA COLIMA" /></a>
+	</figure>
+	<h3 id="slogan">¡<span>Unidos</span> para transformar <span>Colima</span>!</h3>
+	<nav>
+		<ul>
+			<li><a href="quienes_canacintra.html">¿Quienes somos?</a></li>
+			<li><a href="servicios_canacintra.html">Servicios</a></li>
+			<li><a href="red_canacintra.html">Red de Negocios</a></li>
+			<li class="cont"><a href="contacto_canacintra.html">Contacto</a></li>
+			<li class="rs_can"><a href="https://www.facebook.com/canacintra.colima"> <img src="archivos/facebook_canacintra_colima.jpg" alt="Canacintra colima Facebook"></a></li>
+			<li class="rs_can"><a href="https://twitter.com/CANACINTRACOLIM"><img src="archivos/twitter_canacintra_colima.jpg" alt="Canacintra colima Twitter"></a></li>
+		</ul>
+	</nav>
+</header>
+
+
+<section id="contenido_dentro">
+<h2 class="titulo_v2">
+	<figure class="iconos_menu">
+			<img src="archivos/voluntariado_canacintra_colima.png" alt="CANACINTRA colima videos" />
+	</figure>
+			 <p>Voluntariado</p>
+</h2>
+<ul class="menu_quienes">
+	
+	<li><a href="#misionv_info" id="mision">Misión</a></li>
+	<li><a href="#visionv_info" id="vision">Visión</a></li>
+	<li><a href="#objetivov_info" id="general">Objetivo</a></li>
+	<li><a href="#directoriov_info" id="general">Directorio</a></li>
+
+
+</ul>
+
+<section id="info_volun">
+	<article id="voluntariado_info" class="info_voluntariado">
+		<p>“El Voluntariado CANACINTRA esta integrado por personas,</br> </br>que por decisión propia, ofrecen su tiempo,</br></br> sus conocimientos, su experiencia… </br></br>Sin esperar obtener una remuneración de su acción</br></br> pero con la firme convicción de mejorar nuestra sociedad.”</p></article>
+	
+	<article id="misionv_info" class="info_voluntariado">
+	<h3>Misión</h3>
+	<p> Apoyar de manera voluntaria a la sociedad colimense con acciones de interés e impacto, representando a nuestra cámara. 
+	 </p>
+	</article>
+
+	<article id="visionv_info" class="info_voluntariado">
+	<h3>Visión</h3>
+	<p> Ser un órgano de apoyo de la Cámara Nacional de la Industria de la Transformación  reconocido entre la sociedad colimense por su alta responsabilidad social. 
+	Objetivo: Vincular a nuestros agremiados y la sociedad colimense con herramientas como la gestión y el trabajo que permitan el desarrollo de zonas estratégicas y de alta marginación para lograr transformar Colima.  </p>
+	</article>
+	
+	<article id="objetivov_info" class="info_voluntariado">
+	<h3>Objetivo</h3>
+	<p> Vincular a nuestros agremiados y la sociedad colimense con herramientas como la gestión y el trabajo que permitan el desarrollo de zonas estratégicas y de alta marginación para lograr transformar Colima.  </p>
+	</article>
+
+
+<article id="directoriov_info" class="info_voluntariado">
+	<h3>Directorio</h3>
+	<p>Sra. ….. de Alpizar.</p>
+	<p>Presidenta Nacional de Voluntariado CANACINTRA</p>
+	<p>Sra. Mabel Pérez de Carrillo </p>
+	<p>Presidenta del voluntariado CANACINTRA Delegación Colima</p>
+	<p>Sra. Beatriz López García</p>
+	<p>Vicepresidenta responsable del voluntariado </p>
+
+	</article>
+		
+		<img src="archivos/voluntariado_canacintra_colima_presidenta.jpg"  alt="Voluntariado Colima CANACINTRA"/>
+
+
+</section>
+
+
+
+</section>
+
+<article id="menu_opciones">
+	<ul>
+		<li><figure class="iconos_menu">
+			 <a href="archivo_videos_canacintra.php"><img src="archivos/canacintra_tv_colima.png" alt="CANACINTRA colima videos" /></a>
+			</figure>
+			 <a href="archivo_videos_canacintra.php"><p>Canacintra TV</p></a></li>
+		<li><figure class="iconos_menu">
+			<a href="afiliate_canacintra.html"><img src="archivos/afiliate_canacintra_colima.png" alt="CANACINTRA colima afiliate" /></a>
+			</figure>
+			 <a href="afiliate_canacintra.html"><p>Afíliate</p></a></li>
+		<li><figure class="iconos_menu">
+			<a href="voluntariado_canacintra.html"><img src="archivos/voluntariado_canacintra_colima.png" alt="CANACINTRA colima voluntariado" /></a>
+			</figure>
+			 <a href="voluntariado_canacintra.html"><p>Voluntariado</p></a></li>
+		<li><figure class="iconos_menu">
+			 <a href="delegaciones_canacintra.html"><img src="archivos/delegaciones_canacintra_colima.png" alt="CANACINTRA delegaciones" /></a>
+			</figure>
+			 <a href="delegaciones_canacintra.html"><p>Delegaciones</p></a></li>
+	    </ul>
+</article>
+
+
+<aside id="publicidad_inf">
+ <ul>
+  <li><figure class="publicidad_1"><img src="archivos/publi.png" alt="publicidad Colima Industrias"></figure></li>
+  <li><figure class="publicidad_1"><img src="archivos/publi.png" alt="publicidad Colima Industrias"></figure></li>
+  <li><figure class="publicidad_1"><img src="archivos/publi.png" alt="publicidad Colima Industrias"></figure></li>
+</ul>
+</aside>
+
+<section class="infocontacto">
+<p><b>Centro de Negocios PLAZA ZENTRALIA </b> Av. 3er Anillo Periférico 301, Local 240, Col. Valle Dorado, C.P. 28018, Colima, Col. Tel. (312) 32 365 50</p>
+<p><b>Parque Industrial</b> Calle A, Lote 3, Manzana 3, C.P. 28000, Colima, Col. Tels. (312) 30 804 04</p>
+</section>
+
+</div>
+
+
+
+
+</body>
+</html>
